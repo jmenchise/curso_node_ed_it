78d3548e2c5e074bda42dd637fab69c672950036
jmenchise@gmail.com <jmenchise@gmail.com>
2023-05-23 17:19:21 -0300

    prueba con el celular

--- src/app/numerosPrimos.ts
+++ src/app/numerosPrimos.ts
@@ -3,7 +3,8 @@ let primeNumbersArr:number[] = [2,3]
 
 const findPrimeNumbers = (arr:number[]) => {
 
-   for (let number of arr) {  //TODO: cambiar este for of por un for.
+   for (let number of arr) { //TODO: cambiar este for of por un for.
+      console.log(arr) // esto es un cambio que se hizo con el celular.
       if (arr.length > 9) {
          console.log(arr);
          return
