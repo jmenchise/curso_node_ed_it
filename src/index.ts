d374442e672e68374dc38b22a591a7de5f42c883
Joan Menchise <jmenchise@gmail.com>
2023-05-24 21:03:25 -0300

    clase 4 actualizada

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import createFiles from "./app/createFiles"
-import numerosPrimos from "./app/numerosPrimos"
+import { Usuario } from './interfaces/Usuario';
+import genUsuario from './lib/genUsuario'
+import fs from 'fs';
 
 
+let createUserFile = (iteration:number) => {
+   console.log('iteration N°:', iteration);
+   let user:Usuario = genUsuario();
+   console.log('user:', user); 
+   let fileName:string = `./var/arch/${user.id}.json`
+   
+   fs.writeFile(fileName, JSON.stringify(user), 'utf-8',err => {
+      console.log(err? err : '');
+      iteration < 10 && createUserFile(++iteration);
+   })
 
-// createFiles();
-numerosPrimos(25);
+}
 
 
+createUserFile(1);
