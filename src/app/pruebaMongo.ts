17bfc95ebbef4512662c7d1b7dffdcf7b1d0d451
Joan Menchise <jmenchise@gmail.com>
2023-06-12 23:41:57 -0300

    Clase nro 9 primera parte

--- src/app/pruebaMongo.ts
+++ src/app/pruebaMongo.ts
@@ -1,9 +1,14 @@
 import { MongoClient } from 'mongodb'
+import genUsuario from '../lib/genUsuario';
 
 
 export const pruebaMongo = async () => {
-   const url = 'mongodb://0.0.0.0:27017'
-   await MongoClient.connect(url);
-   console.log('Conexión con Mongodb exitosa!')
+   const url = 'mongodb://0.0.0.0:27017';
+   const client = await MongoClient.connect(url);
+   console.log('Conexión con Mongodb exitosa!');
+   const DB = client.db('nodebraker_10');
+   const collection = DB.collection('alumnos');
+   await collection.insertOne(genUsuario());
    
+   await client.close();
 }
